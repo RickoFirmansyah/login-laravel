ddd96066ca3dd8ecc9989f406bafa0a29f354e71
Tio Ramadhani <tioramadhani654@gmail.com>
2024-05-20 16:35:46 +0700

    [STYLE] Update Tampilan Login

--- resources/views/layouts/partials/landing-header.blade.php
+++ resources/views/layouts/partials/landing-header.blade.php
@@ -1,12 +1,13 @@
 <header class="header">
     <nav class="navbar navbar-expand-lg py-0">
         <div class="container">
-            <a class="navbar-brand me-0 py-0" href="index.html">
-                <img src="{{asset("landing/images/logos/logo-erpl.svg")}}" alt="img-fluid">
+            <a class="navbar-brand d-flex align-items-center gap-2" href="index.html">
+                <img src="{{ asset('landing/images/logos/logo.png') }}" alt="img-fluid">
+                <h5 class="fw-bolder">Pantau Ternak</h5>
             </a>
             <button class="navbar-toggler d-none" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <i class="ti ti-menu-2 fs-9"></i>
             </button>
             <button class="navbar-toggler border-0 p-0 shadow-none" type="button" data-bs-toggle="offcanvas"
@@ -16,31 +17,26 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav align-items-center mb-2 mb-lg-0 ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#alur">Alur Pendaftaran</a>
+                        <a class="nav-link active" aria-current="page" href="#alur">Beranda</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#jadwal">Jadwal</a>
+                        <a class="nav-link active" aria-current="page" href="#jadwal">Lokasi Pemantauan</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#biaya">Biaya</a>
+                        <a class="nav-link active" aria-current="page" href="#biaya">Statistik</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"
-                            target="_blank">Unduh</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#faq">FAQ</a>
+                        <a class="nav-link active" aria-current="page" href="#biaya">Berita</a>
                     </li>
                     <li class="nav-item ms-5">
                         <div class="">
                             <ul class="navbar-nav">
                                 <li class="nav-item ">
-                                    <a class="btn fs-3 rounded px-3 py-2"
-                            href="/login">Login</a>
+                                    <a class="btn fs-3 rounded px-3 py-2" href="/login">Login</a>
                                 </li>
                                 <li class="nav-item ms-2">
                                     <a class="btn btn-primary fs-3 rounded btn-hover-shadow px-3 py-2"
-                            href="#">Mendaftar</a>
+                                        href="#">Mendaftar</a>
                                 </li>
                             </ul>
                         </div>
