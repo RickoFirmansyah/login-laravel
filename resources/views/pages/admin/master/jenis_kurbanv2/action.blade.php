40bd5bcdce0ce90fd81bc7108941c65b6894ee53
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2024-06-02 01:36:38 +0700

    update data table di delete data

--- resources/views/pages/admin/master/jenis_kurbanv2/action.blade.php
+++ resources/views/pages/admin/master/jenis_kurbanv2/action.blade.php
@@ -3,7 +3,7 @@
     <a href="{{ route("jenis-kurban.edit", $jenis_kurban->id) }}" class="btn btn-warning">
         <i class="fas fa-pen fs-3"></i>
     </a>
-    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route("jenis-kurban.destroy", $jenis_kurban->id) }}" data-action="delete" data-name="{{ $jenis_kurban->type_of_animal }}" class="btn btn-danger">
+    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route("jenis-kurban.destroy", $jenis_kurban->id) }}" data-table-id="jeniskurban-table" data-action="delete" data-name="{{ $jenis_kurban->type_of_animal }}" class="btn btn-danger">
         <i class="fas fa-trash fs-3"></i>
     </button>
 
