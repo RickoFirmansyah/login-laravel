556dcdc0cd74f8f24b0048c6784b995eb0147b71
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2024-06-02 01:36:06 +0700

    fix update model year

--- app/Models/Year.php
+++ app/Models/Year.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Year extends Model
 {
     use HasFactory;
-    protected $table = 'year';
+    // protected $table = 'year';
     protected $guards = [];
+    protected $fillable = ['tahun', 'status', 'created_by', 'update_by'];
 
-    public function qurban_report() {
-        return $this->hasMany(QurbanReport::class, 'year_id', 'id');
-    }
+    // public function qurban_report() {
+    //     return $this->hasMany(QurbanReport::class, 'year_id', 'id');
+    // }
 }
