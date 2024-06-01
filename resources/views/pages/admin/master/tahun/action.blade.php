ed3db0cc3140bb02aaed213901f812407aa02be2
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2024-06-02 01:36:55 +0700

    update isi dari action tahun

--- resources/views/pages/admin/master/tahun/action.blade.php
+++ resources/views/pages/admin/master/tahun/action.blade.php
@@ -1,9 +1,10 @@
 <div class="d-flex justify-content-center align-items-center gap-2">
 
-    <a href="{{ route("jenis-kurban.edit", $jenis_kurban->id) }}" class="btn btn-warning">
+    <a href="{{ route("tahun.edit", $tahun->id) }}" class="btn btn-warning">
         <i class="fas fa-pen fs-3"></i>
     </a>
-    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route("jenis-kurban.destroy", $jenis_kurban->id) }}" data-action="delete" data-name="{{ $jenis_kurban->type_of_animal }}" class="btn btn-danger">
+    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route("tahun.destroy", $tahun->id) }}"
+        data-table-id="tahun-table" data-action="delete" data-name="{{ $tahun->tahun }}" class="btn btn-danger">
         <i class="fas fa-trash fs-3"></i>
     </button>
 
