60486855b92c25e6f018ca71e5c24d544b16472a
BenedPangestu <ajipangestu4211@gmail.com>
2024-06-04 21:05:16 +0700

    bug fix tempat pemotongan & panduan

--- resources/views/pages/admin/map-pemotongan/action.blade.php
+++ resources/views/pages/admin/map-pemotongan/action.blade.php
@@ -1,10 +1,10 @@
 <div class="d-flex justify-content-center align-items-center gap-2">
 
-    <a href="{{ route("admin.data-pokok.tempat-pemotongan.edit", $tempatPemotongan->id) }}" class="btn btn-warning">
+    <a href="{{ route("admin.tempat-pemotongan.edit", $tempatPemotongan->id) }}" class="btn btn-warning">
         <i class="fas fa-pen fs-3"></i>
     </a>
-    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route("admin.data-pokok.tempat-pemotongan.destroy", $tempatPemotongan->id) }}"
-        data-action="delete" data-table-id="userlist-table" data-name="{{ $tempatPemotongan->name }}"
+    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route("admin.tempat-pemotongan.destroy", $tempatPemotongan->id) }}"
+        data-action="delete" data-table-id="slaughteringplace-table" data-name="{{ $tempatPemotongan->cutting_place }}"
         class="btn btn-danger">
         <i class="fas fa-trash fs-3"></i>
     </button>
--- routes/web.php
+++ routes/web.php
@@ -38,13 +38,13 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('/user-list', UserController::class)->names('user-list');
 
     // PANDUAN
-    Route::resource('/admin/data-pokok/panduan', PanduanController::class)->names('admin.data-pokok.panduan');
+    Route::resource('/admin/panduan', PanduanController::class)->names('admin.panduan');
     
     // TEMPAT PEMOTONGAN
-    Route::resource('/admin/data-pokok/tempat-pemotongan', SlaughteringPlaceController::class)->names('admin.data-pokok.tempat-pemotongan');
-    Route::get('/admin/data-pokok/tempat-pemotongan/kabupaten/{provinsi}', [SlaughteringPlaceController::class, 'getKabupaten'])->name('getKabupaten');
-    Route::get('/admin/data-pokok/tempat-pemotongan/kecamatan/{kabupaten}', [SlaughteringPlaceController::class, 'getKecamatan'])->name('getKecamatan');
-    Route::get('/admin/data-pokok/tempat-pemotongan/kelurahan/{kecamatan}', [SlaughteringPlaceController::class, 'getKelurahan'])->name('getKelurahan');
+    Route::resource('/admin/tempat-pemotongan', SlaughteringPlaceController::class)->names('admin.tempat-pemotongan');
+    Route::get('/admin/tempat-pemotongan/kabupaten/{provinsi}', [SlaughteringPlaceController::class, 'getKabupaten'])->name('getKabupaten');
+    Route::get('/admin/tempat-pemotongan/kecamatan/{kabupaten}', [SlaughteringPlaceController::class, 'getKecamatan'])->name('getKecamatan');
+    Route::get('/admin/tempat-pemotongan/kelurahan/{kecamatan}', [SlaughteringPlaceController::class, 'getKelurahan'])->name('getKelurahan');
     Route::resource('/map-pemotongan', MapController::class)->names('map-pemotongan');
 
     // PETUGAS PEMANTAUAN
