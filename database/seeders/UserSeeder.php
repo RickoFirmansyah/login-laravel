299e1db9742340d239df2d0863d7149593a26871
ridhoriky <ridhoestu.b@gmail.com>
2024-06-08 12:43:22 +0700

    [FIX] add column nomor_hp

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
         $admin = User::create([
             'name' => 'Admin',
             'email' => 'admin@arkatama.test',
+            'nomor_hp' => '081234567890',
             'email_verified_at' => now(),
             'password' => Hash::make('12345'),
             'remember_token' => Str::random(10),
@@ -24,6 +25,7 @@ class UserSeeder extends Seeder
         $operator = User::create([
             'name' => 'Operator',
             'email' => 'operator@arkatama.test',
+            'nomor_hp' => '081234577890',
             'email_verified_at' => now(),
             'password' => Hash::make('12345'),
             'remember_token' => Str::random(10),
@@ -35,6 +37,7 @@ class UserSeeder extends Seeder
             $camaba = User::create([
                 'name' => 'Petugas ' . $i,
                 'email' => 'Petugas' . $i . '@arkatama.test',
+                'nomor_hp' => '0812344' . $i . '890',
                 'email_verified_at' => now(),
                 'password' => Hash::make('12345'),
                 'remember_token' => Str::random(10),
