f07c36a118528797e58ae292ed31a1dd23edc254
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2024-06-10 01:12:24 +0700

    membuat model type penyakit

---
+++ app/Models/TypeOfDiseases.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TypeOfDiseases extends Model
+{
+    use HasFactory;
+}
