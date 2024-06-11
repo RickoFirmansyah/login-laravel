2c7db2b885f566936b6c6b2790ae47d4eb80254d
ridhoriky <ridhoestu.b@gmail.com>
2024-06-11 18:30:45 +0700

    [FIX] action role

--- resources/views/pages/admin/user/role/action.blade.php
+++ resources/views/pages/admin/user/role/action.blade.php
@@ -1,12 +1,14 @@
 <div class="d-flex justify-content-center align-items-center gap-2">
 
-    <a href="{{ route("role.edit", $role->id) }}" class="btn btn-success">
+    <a href="{{ route('role.edit', $role->id) }}" class="btn btn-success">
         <i class="fas fa-pen fs-3"></i>
     </a>
-    
-    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route('role.destroy', $role->id) }}"
-        data-action="delete" data-table-id="roles-table" data-name="{{ $role->name }}"
-        class="btn btn-warning">
+    <a href="{{ route('role.show', $role->id) }}" class="btn btn-info">
+        <i class="fas fa-eye fs-3"></i>
+    </a>
+
+    <button data-csrf-token="{{ csrf_token() }}" data-url="{{ route('role.destroy', $role->id) }}" data-action="delete"
+        data-table-id="roles-table" data-name="{{ $role->name }}" class="btn btn-warning">
         <i class="fas fa-trash fs-3"></i>
     </button>
 
