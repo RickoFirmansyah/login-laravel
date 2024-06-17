b28b8317a492ce86e8834594559fd0b317973c15
BenedPangestu <ajipangestu4211@gmail.com>
2024-06-17 11:48:13 +0700

    update import tempat pmotongan

--- resources/views/pages/admin/data-pokok/tempat-pemotongan/index.blade.php
+++ resources/views/pages/admin/data-pokok/tempat-pemotongan/index.blade.php
@@ -23,12 +23,20 @@
                         <input type="text" data-table-id="slaughteringplace-table" id="searchBox" data-action="search"
                             class="form-control form-control-solid w-250px ps-13" placeholder="Cari Tempat Pemotongan" />
                     </div>
-                    <a href="{{ route('admin.data-pokok.tempat-pemotongan.create') }}" class="btn btn-primary">
-                        <i class="fas fa-plus fa-sm text-white-50"></i>
-                        <span class="ms-2">
-                            Tambah Tempat Pemotongan
-                        </span>
-                    </a>
+                    <div>
+                        <a href="{{ route('admin.data-pokok.tempat-pemotongan.create') }}" class="btn btn-primary py-2">
+                            <i class="fas fa-plus fa-sm text-white-50"></i>
+                            <span class="ms-2">
+                                Tambah Tempat Pemotongan
+                            </span>
+                        </a>
+                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fas fa-file fa-sm text-white-50" aria-hidden="true"></i>
+                            <span class="ms-2">
+                                Import
+                            </span>
+                        </button>
+                    </div>
                 </div>
                 <div class="table-responsive">
                     {{ $dataTable->table() }}
@@ -36,6 +44,32 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-lg">
+          <div class="modal-content rounded-1">
+            <div class="modal-header border-bottom">
+              {{-- <input type="search" class="form-control fs-3" placeholder="Search here" id="search" /> --}}
+              {{-- <span data-bs-dismiss="modal" class="lh-1 cursor-pointer">
+                <i class="ti ti-x fs-5 ms-3"></i>
+              </span> --}}
+              <span data-bs-dismiss="modal" class="lh-1 cursor-pointer">
+                {{-- <i class="ti ti-x fs-5 ms-3"></i> --}}
+                Import Tepat Pemotongan
+              </span>
+            </div>
+            <div class="modal-body message-body" data-simplebar="">
+                <form action="{{route('admin.data-pokok.tempat-pemotongan.import')}}" method="post">
+                    @csrf
+                    <input type="file" class="form-control fs-3" name="file" id="file" />
+
+                    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+                </form>
+
+            </div>
+            <div class="modal-footer border-top"></div>
+          </div>
+        </div>
+      </div>
 
     @push('scripts')
         {{ $dataTable->scripts() }}
